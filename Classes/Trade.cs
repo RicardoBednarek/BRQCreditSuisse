7e846c3ce52a5924e5e49ef645bd1ee39102bada
Ricardo Brunoro <Ricardo Brunoro@DESKTOP-BT12EC2>
2024-12-06 11:26:05 -0300

    Add project files.

---
+++ Classes/Trade.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BRQCS.Interfaces;
+
+namespace BRQCS.Classes
+{
+    class Trade : ITrade
+    {
+        public double Value { get; }
+        public string ClientSector { get; }
+        public DateTime NextPaymentDate { get; }
+
+        public Trade(double value, string clientSector, DateTime nextPaymentDate)
+        {
+            Value = value;
+            ClientSector = clientSector;
+            NextPaymentDate = nextPaymentDate;
+        }
+
+        public string Calculate(DateTime refDate)
+        {
+            if ((refDate.Date - NextPaymentDate.Date).Days > 30)
+                return "EXPIRED";
+
+            if (ClientSector != "Public" && ClientSector != "Private")
+                return "ERROR";
+
+            if (Value > 1000000 && ClientSector == "Private")
+                return "HIGHRISK";
+
+            if (Value > 1000000 && ClientSector == "Public")
+                return "MEDIUMRISK";
+
+            return "-";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BRQCS.Classes;
+
+namespace BRQCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo culture = new CultureInfo("en-US");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+
+            List<string> response = new List<string>();
+            DateTime reference;
+            int n;
+
+            string refDate = Console.ReadLine();
+            if (!DateTime.TryParse(refDate, out reference))
+            {
+                Console.WriteLine("ERROR");
+                Console.ReadKey();
+                return;
+            }
+
+            string numberTrades = Console.ReadLine();
+            if (!int.TryParse(numberTrades, out n))
+            {
+                Console.WriteLine("ERROR");
+                Console.ReadKey();
+                return;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tradeInfo = Console.ReadLine().Split(' ');
+
+                try
+                {
+                    Trade trade = new Trade(Convert.ToDouble(tradeInfo[0]), tradeInfo[1], Convert.ToDateTime(tradeInfo[2]));
+                    response.Add(trade.Calculate(reference));
+                }
+                catch(Exception ex)
+                {
+                    response.Add("ERROR");
+                }
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < response.Count; i++)
+            {
+                Console.WriteLine(response[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
